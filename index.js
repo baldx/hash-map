8b03b17861d69f53186b8da51088a752b32b6835
baldx <mr.ahmad.m2006@gmail.com>
2024-03-30 22:27:09 +0100

    add logic to set value in head list

--- index.js
+++ index.js
@@ -21,11 +21,57 @@ class LinkedList {
     }
 }
 
-class Node {
+class node {
     constructor(value, next = null) {
         this.value = value;
         this.next = next;
     }
 }
 
-console.log(new HashMap);
+function hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+ 
+    return hashCode;
+} 
+
+let hashMap = new HashMap();
+
+
+function set(key, value) {
+    let hashedKey = hash(key);
+    const length = 16;
+    const index = hashedKey % length;
+    let current = hashMap.capacity[index];
+
+    if (current.head === null) {
+        const newValue = new node(value);
+        current.head = newValue.value
+    }
+
+    return current
+}
+
+console.log(set("asd", "adsd"));
+console.log(hashMap);
+
+/*
+    function set() {
+        if list head is null {
+            set value to head
+            set head.next to null
+        }
+            
+        else {
+            while next node is not null {
+                set current node to next node
+            }
+            set next node to value
+        }
+
+    }
+*/
